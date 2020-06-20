ac88cb264f76a9fc642dd409412a75ad2ebeaa36
Dari Alves <darieltondf@gmail.com>
2020-06-20 19:07:24 -0300

    bg-changer-project

--- bg-changer-project/index.js
+++ bg-changer-project/index.js
@@ -1,13 +1,22 @@
 const button = document.getElementById("btn-change")
 const p = document.querySelector('p');
 
-button.addEventListener('click', changeColor);
+// button.addEventListener('click', changeColor);
 
-function changeColor() {
+// function changeColor() {
+//     const red = Math.round(Math.random() * 256); // devuelve un número aleatorio entre 0 y 255
+//     const green = Math.round(Math.random()* 256);
+//     const blue = Math.round(Math.random() * 256);
+//     const alpha = Math.random();
+//     document.body.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+//     p.innerHTML = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+// }   
+
+button.addEventListener('click', () => {
     const red = Math.round(Math.random() * 256); // devuelve un número aleatorio entre 0 y 255
     const green = Math.round(Math.random()* 256);
     const blue = Math.round(Math.random() * 256);
-    const alpha = Math.random(0,1);
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}, ${alpha})`;
+    const alpha = Math.random();
+    document.body.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
     p.innerHTML = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
-}   
+})
